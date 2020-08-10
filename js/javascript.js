5d69fb101dec8975f167fecd36330aa05e674126
Rodolfo-Rodrigues <50324906+Rodolfo-Rodrigues@users.noreply.github.com>
2020-08-09 23:03:38 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,4 @@
+$('.carousel').carousel({
+    interval: 10000, //muda de um slide para outro a cada 10 segundos
+    pause: false //nao pausa o ciclo do slider quando o cursor do mouse está em cima do slider
+})
